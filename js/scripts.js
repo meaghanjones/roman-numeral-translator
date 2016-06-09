cdd6726c8a044a83404c7f14b24ada20e07e6218
Brett New and Meaghan Jones <brett@>
2016-06-09 14:36:56 -0700

    fix regular expression so non roman numeral input show and alert

--- js/scripts.js
+++ js/scripts.js
@@ -2,58 +2,60 @@
 var romanNumeral = function(numeralInput) {
   var numeralInput = numeralInput.toUpperCase();
   var numeralsArray = numeralInput.split("");
-
-  // if (/[^cdivxlm | (1-9) | (" ")]/ig.test(numeralsArray)) {
-  //   alert ("Please enter a single word with no spaces");
-  //   // {break;}
-  // } else {
-    for (var index = 0; index < numeralsArray.length; index++){
-        var indexNumerals = numeralsArray[index];
-        if (indexNumerals === "I"){
-          numeralsArray[index] = '1';
-        } else if (indexNumerals === "V"){
-          numeralsArray[index] = '5';
-        } else if (indexNumerals === "X"){
-          numeralsArray[index] = '10';
-        }  else if (indexNumerals === "L"){
-          numeralsArray[index] = '50';
-        } else if (indexNumerals === "C"){
-          numeralsArray[index] = '100';
-        } else if (indexNumerals === "D"){
-          numeralsArray[index] = '500';
-        } else if (indexNumerals === "M"){
-          numeralsArray[index] = '1000';
-        };
-        };
-      };
   var answer = 0;
-  debugger;
-  var answer = answer + parseInt(numeralsArray[numeralsArray.length-1]);
-  if (parseInt(numeralsArray[numeralsArray.length-2]) >= parseInt(numeralsArray[numeralsArray.length-1])){
-     var answer = answer + parseInt(numeralsArray[numeralsArray.length-2]);
-  } else if (parseInt(numeralsArray[numeralsArray.length-2]) < parseInt(numeralsArray[numeralsArray.length-1])){
-     var answer = answer - parseInt(numeralsArray[numeralsArray.length-2]);
-  };
-  if (parseInt(numeralsArray[numeralsArray.length-3]) >= parseInt(numeralsArray[numeralsArray.length-2])){
-    var answer = answer + parseInt(numeralsArray[numeralsArray.length-3]);
-  } else if (parseInt(numeralsArray[numeralsArray.length-3]) < parseInt(numeralsArray[numeralsArray.length-2])){
-    var answer = answer - parseInt(numeralsArray[numeralsArray.length-3]);
-  };
-    if (parseInt(numeralsArray[numeralsArray.length-4]) >= parseInt(numeralsArray[numeralsArray.length-3])){
-      var answer = answer + parseInt(numeralsArray[numeralsArray.length-4]);
-  } else if (parseInt(numeralsArray[numeralsArray.length-4]) < parseInt(numeralsArray[numeralsArray.length-3])){
-      var answer = answer - parseInt(numeralsArray[numeralsArray.length-4]);
-  };
-  if (parseInt(numeralsArray[numeralsArray.length-5]) >= parseInt(numeralsArray[numeralsArray.length-4])){
-    var answer = answer + parseInt(numeralsArray[numeralsArray.length-5]);
-  } else if (parseInt(numeralsArray[numeralsArray.length-5]) < parseInt(numeralsArray[numeralsArray.length-4])){
-    var answer = answer - parseInt(numeralsArray[numeralsArray.length-5]);
-  } else (/[^cdivxlm | (1-9) | (" ")]/ig.test(numeralsArray)) {
-    alert ("Please enter a single word with no spaces");
-  };
 
-  return answer;
-  };
+  if (/[abefghjknopqrstuwyz]/ig.test(numeralsArray)) {
+    alert("Please enter a single word with no spaces");
+  }
+
+  else {
+  for (var index = 0; index < numeralsArray.length; index++){
+      var indexNumerals = numeralsArray[index];
+      if (indexNumerals === "I"){
+        numeralsArray[index] = '1';
+      } else if (indexNumerals === "V"){
+        numeralsArray[index] = '5';
+      } else if (indexNumerals === "X"){
+        numeralsArray[index] = '10';
+      }  else if (indexNumerals === "L"){
+        numeralsArray[index] = '50';
+      } else if (indexNumerals === "C"){
+        numeralsArray[index] = '100';
+      } else if (indexNumerals === "D"){
+        numeralsArray[index] = '500';
+      } else if (indexNumerals === "M"){
+        numeralsArray[index] = '1000';
+      }
+    }
+
+    var answer = answer + parseInt(numeralsArray[numeralsArray.length-1]);
+    if (parseInt(numeralsArray[numeralsArray.length-2]) >= parseInt(numeralsArray[numeralsArray.length-1])){
+       var answer = answer + parseInt(numeralsArray[numeralsArray.length-2]);
+    } else if (parseInt(numeralsArray[numeralsArray.length-2]) < parseInt(numeralsArray[numeralsArray.length-1])){
+       var answer = answer - parseInt(numeralsArray[numeralsArray.length-2]);
+    }
+    if (parseInt(numeralsArray[numeralsArray.length-3]) >= parseInt(numeralsArray[numeralsArray.length-2])){
+      var answer = answer + parseInt(numeralsArray[numeralsArray.length-3]);
+    }
+    else if (parseInt(numeralsArray[numeralsArray.length-3]) < parseInt(numeralsArray[numeralsArray.length-2])){
+      var answer = answer - parseInt(numeralsArray[numeralsArray.length-3]);
+    }
+      if (parseInt(numeralsArray[numeralsArray.length-4]) >= parseInt(numeralsArray[numeralsArray.length-3])){
+        var answer = answer + parseInt(numeralsArray[numeralsArray.length-4]);
+    }
+    else if (parseInt(numeralsArray[numeralsArray.length-4]) < parseInt(numeralsArray[numeralsArray.length-3])){
+        var answer = answer - parseInt(numeralsArray[numeralsArray.length-4]);
+    }
+    if (parseInt(numeralsArray[numeralsArray.length-5]) >= parseInt(numeralsArray[numeralsArray.length-4])){
+      var answer = answer + parseInt(numeralsArray[numeralsArray.length-5]);
+    }
+
+    else if (parseInt(numeralsArray[numeralsArray.length-5]) < parseInt(numeralsArray[numeralsArray.length-4])){
+      var answer = answer - parseInt(numeralsArray[numeralsArray.length-5]);
+    }
+    return answer;
+  }
+}
 
 
 //user interface logics
